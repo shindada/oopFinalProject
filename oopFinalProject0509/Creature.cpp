a46fab9b99be95cfe23044650fdc2127fe98cced
ReMi-Hsu <b10732016@gapps.ntust.edu.tw>
2020-05-14 14:31:27 +0800

    init

---
+++ oopFinalProject0509/Creature.cpp
@@ -0,0 +1,32 @@
+﻿#include "Creature.h"
+//Creature::Creature(const int& hpTemp, const int& defTemp)
+//	:totalHp(hpTemp), hp(hpTemp), def(defTemp), shield(0) {
+//}
+void Creature::heal(const int& bloodTemp) {
+	if (hp + bloodTemp < totalHp) {
+		totalHp = hp + bloodTemp;
+	}
+}
+void Creature::shieldPlus(const int& shieldTemp) {
+	shield = shieldTemp;
+	def += shield;
+}
+const int& Creature::getTotalHp() const {
+	return totalHp;
+}
+const int& Creature::getHp() const {
+	return hp;
+}
+const int& Creature::getDef() const {
+	return def;
+}
+const int& Creature::getShield() const {
+	return shield;
+}
+void Creature::shieldToZero() {
+	def -= shield;
+	shield = 0;
+}
+const Pos& Creature::getPosition() const {
+	return position;
+}
---
+++ oopFinalProject0509/Monster.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Creature.h"
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+//#include "Creature.h"
+#include "Func.h"
+class Monster :public Creature {
+private:
+	int chooseCard;
+	static char alpha;
+	char name;
+	int status[3];//hp atk range
+	int chooseNow;
+	std::vector <MonsterCardData> holdCard;
+public:
+	Monster(const std::vector <MonsterData>& mD,int people);
+	void draw();//抽牌
+	void turn();
+	int attack(int atkTemp);
+	int attack(int atkTemp, int rangeTemp);
+	void move(std::string commandTemp);
+	void shuffle();//洗牌
+};
