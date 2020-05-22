60d13683aa6306c9c33e7f292bbbbdeb129b9304
ReMi-Hsu <b10732016@gapps.ntust.edu.tw>
2020-05-22 13:49:09 +0800

    d

--- 0520/0520/Board.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Readfile.h"
-
-void iniGamerBoard(void);
-void findRoom(int level, std::vector<Position>& door);
-void reFindRoom(bool** isValid, Position start, std::vector<Position>& door);
-void outputGamerBoard(void);
-void delGamerBoard(void);
